16fcfa5718477e2f91a83b2ca177a3c73f993125
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-24 17:00:48 +0000

    new buy health function for rpg1 js

--- freecodecamp/rpg1/script.js
+++ freecodecamp/rpg1/script.js
@@ -73,3 +73,14 @@ function goCave() {
 function fightDragon() {
   console.log("Fighting dragon.");
 }
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
