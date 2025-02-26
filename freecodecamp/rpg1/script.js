820bddbde9fd57f54708f68982563656ca7198ec
Esat Gokcen <esatgokcen173@gmail.com>
2025-02-26 23:03:42 +0000

    updated fight functions in rpg1 js

--- freecodecamp/rpg1/script.js
+++ freecodecamp/rpg1/script.js
@@ -119,9 +119,16 @@ function sellWeapon() {
 }
 
 function fightSlime() {
-
+  fighting = 0;
+  goFight();
 }
 
 function fightBeast() {
+  fighting = 1;
+  goFight();
+}
 
+function fightDragon() {
+  fighting = 2;
+  goFight();
 }
