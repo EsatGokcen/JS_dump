e7c356a82b8338a3375149e130987130c7dcd955
Esat Gokcen <esatgokcen173@gmail.com>
2025-03-21 22:21:08 +0000

    completed calculate calories function for calorie counter

--- freecodecamp/calorieCounter/script.js
+++ freecodecamp/calorieCounter/script.js
@@ -39,7 +39,7 @@ function addEntry() {
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
-  
+
   const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
   const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
   const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
@@ -52,11 +52,25 @@ function calculateCalories(e) {
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
   const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
   if (isError) {
-    return null; 
+    return;
   }
 
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
+  `;
+
+  output.classList.remove('hide');
 }
+
 
 function getCaloriesFromInputs(list) {
   let calories = 0;
